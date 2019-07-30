4153f369efd12c30bc5b501f7725268a8349bb7f
Bri Aguilar <breezy.aguilar@gmail.com>
2019-07-29 21:26:02 -0500

    Controller setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+// Import Express
+var express = require("express");
+
+// Creating router
+var router = express.Router();
+
+// Import burger.js
+var burger = require("../models/burger.js");
+
+
+
+
+
+// Exporting router
+module.exports = router;
