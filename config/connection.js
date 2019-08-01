7154bb08e6e2c168202e97783dc75d19714fc2b0
Bri Aguilar <breezy.aguilar@gmail.com>
2019-07-31 21:13:38 -0500

    Heroku connection.js credentials installed

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,22 @@
 // Setting up MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    mysql_native_password: "password",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    })
+}
 
 // Make connection
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for ORM
